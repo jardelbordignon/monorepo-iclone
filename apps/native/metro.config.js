a1bd1c15af3bc1e572f1b005a44de161f162159f
Jardel Bordignon <jardel1101@gmail.com>
2023-08-13 21:56:13 -0300

    native - expo 49

--- apps/native/metro.config.js
+++ apps/native/metro.config.js
@@ -1,28 +1,7 @@
-const path = require('path')
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('expo/metro-config');
 
-const { getDefaultConfig } = require('expo/metro-config')
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname);
 
-// Find the project and workspace directories
-const projectRoot = __dirname
-// This can be replaced with `find-yarn-workspace-root`
-const workspaceRoot = path.resolve(projectRoot, '../..')
-
-const config = getDefaultConfig(projectRoot, {
-  // isCSSEnabled: true,
-})
-
-// 1. Watch all files within the monorepo
-config.watchFolders = [workspaceRoot]
-// 2. Let Metro know where to resolve packages and in what order
-config.resolver.nodeModulesPaths = [
-  path.resolve(projectRoot, 'node_modules'),
-  path.resolve(workspaceRoot, 'node_modules'),
-]
-// 3. Force Metro to resolve (sub)dependencies only from the `nodeModulesPaths`
-config.resolver.disableHierarchicalLookup = true
-
-// Expo 49 issue: default metro config needs to include "mjs"
-// https://github.com/expo/expo/issues/23180
-config.resolver.sourceExts.push('mjs')
-
-module.exports = config
+module.exports = config;
