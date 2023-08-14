be7a34a28a1c34471546b332316da6659bbd67eb
Jardel Bordignon <jardel1101@gmail.com>
2023-08-13 21:57:06 -0300

    web - svg support

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,12 +1,23 @@
 // https://vitejs.dev/config/
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      'react-native': 'react-native-web',
+import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite'
+import svgr from 'vite-plugin-svgr'
+
+export default ({ mode }: { mode: string }) => {
+  // Load app-level env vars to node-level env vars.
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+
+  return defineConfig({
+    //  envPrefix: 'EXPO_PUBLIC_',
+    //  envDir: resolve(__dirname, '..', '..', 'packages', 'front'),
+    plugins: [react(), svgr({ exportAsDefault: true })],
+    resolve: {
+      alias: {
+        'react-native': 'react-native-web',
+        'react-native-svg': 'react-native-web-svg',
+      },
     },
-  },
-})
+  })
+}
