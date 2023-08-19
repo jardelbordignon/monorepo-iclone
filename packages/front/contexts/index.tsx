e8bd93c3e94e52b8daa3ee38ed2f1787caf6df27
Jardel Bordignon <jardel1101@gmail.com>
2023-08-18 21:14:41 -0300

    remove react-query

--- packages/front/contexts/index.tsx
+++ packages/front/contexts/index.tsx
@@ -3,7 +3,6 @@ import type { ReactNode } from 'react'
 
 import { AccountProvider } from './account'
 import { LocalesProvider } from './locales'
-import { ReactQueryProvider } from './react-query'
 
 type Props = {
   children: ReactNode
@@ -13,9 +12,7 @@ export function Contexts({ children }: Props) {
   return (
     <Suspense fallback={null}>
       <LocalesProvider>
-        <AccountProvider>
-          <ReactQueryProvider>{children}</ReactQueryProvider>
-        </AccountProvider>
+        <AccountProvider>{children}</AccountProvider>
       </LocalesProvider>
     </Suspense>
   )
